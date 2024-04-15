c5f70b1b1167cb8b28e285d243242625ae9ebb3a
kikiandalucia <3143869032@qq.com>
2024-04-15 19:37:25 +0800

    Site updated: 2024-04-15 19:37:24

--- js/randombackground.js
+++ js/randombackground.js
@@ -5,6 +5,19 @@ var backimg =[
     "url(https://www.z4a.net/images/2024/04/13/wallhaven-d6dvdl.png)",
   
     "url(https://www.z4a.net/images/2024/04/14/wallhaven-exwgmr.png)",
+    
+    "url(https://www.z4a.net/images/2024/04/15/Lain-Iwakura-Serial-Experiments-Lain-1385665-wallhere.com.png)",
+    "url(https://www.z4a.net/images/2024/04/15/anime-girls-Lycoris-Recoil-profile-hands-in-pockets-2241876-wallhere.come52ece649a6a0414.jpg)",
+    "url(https://www.z4a.net/images/2024/04/15/anime-2288760-wallhere.com.jpg)",
+    "url(https://www.z4a.net/images/2024/04/15/sky-anime-2300384-wallhere.com.png)",
+    "url(https://www.z4a.net/images/2024/04/15/anime-anime-girls-Gracile-landscape-blurred-city-lights-2301208-wallhere.com.jpg)",
+    "url(https://www.z4a.net/images/2024/04/15/anime-girls-Lycoris-Recoil-hands-in-pockets-2241878-wallhere.com387ede3a2d27ca41.jpg)",
+    "url(https://www.z4a.net/images/2024/04/15/anime-girls-fireworks-sky-anime-sky-clouds-2271428-wallhere.com.jpg)",
+    "url(https://www.z4a.net/images/2024/04/15/anime-girls-anime-nimbus-gun-birds-animals-2200469-wallhere.com.jpg)",
+    "url(https://www.z4a.net/images/2024/04/15/Pixiv-anime-2249634-wallhere.com.jpg)",
+    "url(https://www.z4a.net/images/2024/04/15/anime-anime-girls-2298976-wallhere.com.jpg)",
+    "url(https://www.z4a.net/images/2024/04/15/anime-anime-girls-2299014-wallhere.com.jpg)",
+    "url(https://www.z4a.net/images/2024/04/15/photo-manipulation-city-cityscape-night-Moon-women-2262610-wallhere.com.jpg)",
   
   ];
   
